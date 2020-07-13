6847bdfe74ed037c225750469fdda9b691b4fac8
Yaseen Syed <yaseenzsyed@gmail.com>
2020-07-12 23:27:19 -0500

    Add files via upload

---
+++ Cosc 4353/Serverside/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"quickstart/controller"
+
+)
+
+
+func main(){
+	fmt.Println("Starting...")
+	r := mux.NewRouter()
+	r.HandleFunc("/register", controller.RegisterHandler).Methods("POST")
+	r.HandleFunc("/login", controller.LoginHandler).Methods("POST")
+	r.HandleFunc("/profileInfo",controller.GetProfileInfo).Methods("GET")
+	r.HandleFunc("/profileSetter",controller.InsertProfileInfo).Methods("PUT")
+	r.HandleFunc("/fuelQuoteForm", controller.DeliveryRequestHandler).Methods("PUT")
+	r.HandleFunc("/fuelQuoteHistory", controller.GetDeliveryRequests).Methods("GET")
+	fmt.Println("nice!")
+	http.ListenAndServe(":8000",r)
+}
